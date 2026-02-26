ff7b436048923795913e690a3ef765b4223bc3de
Nouman Ashraf <2502124@students.au.edu.pk>
2026-02-25 20:51:48 -0800

    Add files via upload
    These are my tasks of lab 1

---
+++ main.js
@@ -0,0 +1,6 @@
+
+import { add, subtract } from './math-utils.js';
+const sum = add(10, 5);
+const difference = subtract(20, 7);
+console.log("The sum of 10 + 5 is:", sum);          
+console.log("The difference of 20 - 7 is:", difference); 
